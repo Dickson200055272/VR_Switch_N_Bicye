4922de4cd9c1170ab3c57f5455409818d753da33
Dickson200055272 <200055272@stu.vtc.edu.hk>
2021-08-11 16:43:44 +0800

    Day2TestingJoyCon1

---
+++ VR_X_V1B1/Assets/JoyConComConOut.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyConComConOut : MonoBehaviour
+{
+
+    public float GetJCX; 
+    public float PositX;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gyro = j.GetGyro() = GetJCX;
+
+        
+        /*object.tag("RunBar")
+        {
+        if (GetJCX > 1)
+        {
+            PositX == GetJCX;
+        }
+            transform.position = new Vector3(PositX, position.y);
+        }*/
+
+    }
+
+ 
+}
